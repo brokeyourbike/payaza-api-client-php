13bd51fbb8e61e2179cd8bbd5bca74300835999a
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-10-23 09:53:06 +0100

    fix: add more statuses

--- src/Enums/TransactionStatusEnum.php
+++ src/Enums/TransactionStatusEnum.php
@@ -11,6 +11,24 @@ namespace BrokeYourBike\Payaza\Enums;
  */
 enum TransactionStatusEnum: string
 {
+    // transaction has been received and queued for processing
     case TRANSACTION_INITIATED = 'TRANSACTION_INITIATED';
+
+    // amount has been deducted, 
+    // but it is being processed by the bank, 
+    // and it can be reversed due to network problems
     case ESCROW_SUCCESS = 'ESCROW_SUCCESS';
+
+    // transaction is successful
+    case NIP_SUCCESS = 'NIP_SUCCESS';
+
+    // transaction has failed
+    case NIP_FAILURE = 'NIP_FAILURE';
+
+    // transaction is still in progress
+    case NIP_PENDING = 'NIP_PENDING';
+
+    // transaction fails at the bank, 
+    // it is automatically reversed and credited back to your account
+    case NIP_REVERSAL_SUCCESS = 'NIP_REVERSAL_SUCCESS';
 }
